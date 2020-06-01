4d6d8c893436c61be0838d93b9a8b4bf89eb899d
DacHuy <nguyendachuy5326@gmail.com>
2020-06-01 14:25:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import StopWatch from './StopWatch';
+import StopWatchDraft from './StopWatchDraft';
+import StopWatchDraft2 from './StopWatchDraft2';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => StopWatch);
